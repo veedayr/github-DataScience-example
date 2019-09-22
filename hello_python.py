33f1e5a3e460bffdedab1a39e42465337d1ae4b9
Sriramachandra Dhulipala <SD00477193@techmahindra.com>
2019-09-22 11:53:51 +0530

    added coursera work

--- hello_python.py
+++ hello_python.py
@@ -1,1 +1,12 @@
 print("Hello Github!")
+<h1 id="pd"> Pandas Library </h1>
+<p>
+The Pandas Library is a useful tool that enables us to read various datasets into a data frame; our Jupyter notebook platforms have a built-in <b>Pandas Library</b> so that all we need to do is import Pandas without installing.
+</p>
+# Import pandas library
+import pandas as pd
+
+# Import numpy library
+import numpy as np
+
+print("Hello Capstone Project Course!")
